80ea8d1e4447c3da75553d10115af6370f9751cf
shahidalamkkbk <143724326+shahidalamkkbk@users.noreply.github.com>
2024-01-25 11:04:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1767,7 +1767,7 @@ async def auto_filter(client, msg, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>🎬Mᴏᴠɪᴇs Nᴀᴍᴇ :- [{search}]\n🙎Rᴇǫᴜᴇsᴛ Bʏ :- [{message.from_user.mention}]\n💥Sʜᴏᴡ Rᴇsᴜʟᴛ Tɪᴍᴇ :- [{remaining_seconds}] sᴇᴄᴏɴᴅs\n👪Gʀᴏᴜᴘ :- [{message.chat.title}] \n\n📎Pᴏᴡᴇʀᴇᴅ Bʏ :- <ahref=https://t.me/Stark_Botz7>Sᴛᴀʀᴋ Bᴏᴛᴢ™</a>\n\n</b>"
+            cap = f"<b>🎬Mᴏᴠɪᴇs Nᴀᴍᴇ :- [{search}]\n🙎Rᴇǫᴜᴇsᴛ Bʏ :- [{message.from_user.mention}]\n💥Sʜᴏᴡ Rᴇsᴜʟᴛ Tɪᴍᴇ :- [{remaining_seconds}] sᴇᴄᴏɴᴅs\n👪Gʀᴏᴜᴘ :- [{message.chat.title}] \n\n📎Pᴏᴡᴇʀᴇᴅ Bʏ :- <a href=https://t.me/Stark_Botz7>Sᴛᴀʀᴋ Bᴏᴛᴢ™</a>\n\n</b>"
         else:
             cap = f"<b>Hᴇʏ {message.from_user.mention}, Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs ғᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}\n\n</b>"
             cap+="<b><u>📚 Requested Files 👇</u></b>\n\n"
